4a5ec3302c720631ff589d60bd8e7468119c26d6
Thibaud Coroller <14895385+tcoroller@users.noreply.github.com>
2026-03-17 21:47:02 -0400

    FIX: import bug

--- docs/conf.py
+++ docs/conf.py
@@ -8,11 +8,11 @@
 import os
 import sys
 
-import torchsurv
-
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath("../src"))
+
+import torchsurv
 
 # -- Project information -----------------------------------------------------
 
